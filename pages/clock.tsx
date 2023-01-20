2570bf1342dd58de2e7fbbb4368f3f9cfe0ce91a
Keshav Bohra <keshav2016@gmail.com>
2023-01-21 02:48:15 +0530

    added markings in clock

--- pages/clock.tsx
+++ pages/clock.tsx
@@ -4,6 +4,13 @@ export default function Clock() {
     return <div>
         <div className={styles.clock_container}>
             <div className={styles.clock}>
+                <div className={`${styles.marking}`}></div>
+                <div className={`${styles.horizontal_marking} ${styles.marking}`}></div>
+                <div className={`${styles.marking} ${styles.other_marking} ${styles.marking_a}`}></div>
+                <div className={`${styles.marking} ${styles.other_marking} ${styles.marking_b}`}></div>
+                <div className={`${styles.marking} ${styles.other_marking} ${styles.marking_c}`}></div>
+                <div className={`${styles.marking} ${styles.other_marking} ${styles.marking_d}`}></div>
+                <div className={`${styles.inner_circle}`}></div>
                 <div className={styles.clock_centre}></div>
                 <div className={`${styles.clock_hour_hand} ${styles.hand}`}></div>
                 <div className={`${styles.clock_minute_hand} ${styles.hand}`}></div>
